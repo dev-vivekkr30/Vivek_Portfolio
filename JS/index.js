3a365d8da30122d75a8de6e763e69f0d7ae76d1b
dev-vivekkr30 <dev.vivekkr30@gmail.com>
2023-07-31 13:49:15 +0530

    changed email fun

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,24 @@
 function sendMail(){
+    alert("Function called")
     var params = {
         name : document.getElementById("f_name").value,
         email : document.getElementById("email_id").value,
         message : document.getElementById("message").value
     }
 
-    emailjs.send("service_9u1hfzj","template_mpmmi0j",params).then(function res(){
-        alert("Success!" + res.status);
+    const serviceID = "service_9u1hfzj";
+    const templateID= "template_mpmmi0j";
+
+    emailjs.send(serviceID, templateID ,params)
+    .then((res) => {
+        document.getElementById("f_name").value = "";
+        document.getElementById("email_id").value = "";
+        document.getElementById("message").value = "";
+
+        console.log(res);
+        alert("Message Sent");
     })
+
+    .catch((err) => console.log(err));
+
 }
